23260be319111db65cef769c10c7d3f3e9416236
Mario Junior <drapalskimario@gmail.com>
2023-06-21 19:09:03 -0300

    add implementacao de Retry em transaction-bff

--- transaction-bff/src/main/java/br/com/mdrapalski/transactionbff/domain/TransactionService.java
+++ transaction-bff/src/main/java/br/com/mdrapalski/transactionbff/domain/TransactionService.java
@@ -3,14 +3,17 @@ package br.com.mdrapalski.transactionbff.domain;
 import br.com.mdrapalski.transactionbff.dto.RequestTransactionDto;
 import br.com.mdrapalski.transactionbff.dto.TransactionDto;
 import br.com.mdrapalski.transactionbff.redis.TransactionRedisRepository;
+import org.springframework.dao.QueryTimeoutException;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@Service
+@Service 
 public class TransactionService {
 
     private final TransactionRedisRepository transactionRedisRepository;
@@ -25,6 +28,7 @@ public class TransactionService {
         return Optional.of(this.transactionRedisRepository.save(requestTransactionDto));
     }
 
+    @Retryable(retryFor = QueryTimeoutException.class, maxAttempts = 5, backoff = @Backoff(delay = 100))
     public Optional<TransactionDto> findById(final String id) {
         return this.transactionRedisRepository.findById(id);
     }
